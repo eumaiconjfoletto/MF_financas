02ccb6523347f52182d17b1bb49e32ce6bc91abd
MAICON JULIANO FOLETTO <maiconjfoletto@gmail.com>
2026-06-04 21:35:06 -0300

    Update sistema.js

--- sistema.js
+++ sistema.js
@@ -1,8 +1,8 @@
 /* ==========================================================================
       CONFIGURAÇÃO CENTRAL E INSTÂNCIA DO SUPABASE
    ========================================================================== */
-const SUPABASE_URL = "https://hrdtylxfkcsgyhghhptr.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZHR5bHhma2NzZ3loZ2hocHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc3NzksImV4cCI6MjA5NTU2Mzc3OY0.DwnwVrOcYfTeiz18BJEvCipoeDUQ4t_dg3biuCtIe94";
+const SUPABASE_URL = "https://hrdtylxfkcsgyhghhptr.supabase.co/rest/v1/";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZHR5bHhma2NzZ3loZ2hocHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc3NzksImV4cCI6MjA5NTU2Mzc3OX0.DwnwVrOcYfTeiz18BJEvCipoeDUQ4t_dg3biuCtIe94";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
