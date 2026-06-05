b738e37da9a2199581491c111c95f2db613e6684
MAICON JULIANO FOLETTO <maiconjfoletto@gmail.com>
2026-06-05 15:24:14 -0300

    Update sistema.js

--- assets/js/sistema.js
+++ assets/js/sistema.js
@@ -11,7 +11,7 @@ if (typeof supabase === 'undefined') {
    CONFIGURAÇÃO CENTRAL E INSTÂNCIA DO SUPABASE
    ========================================================================== */
 const SUPABASE_URL = "https://hrdtylxfkcsgyhghhptr.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZHR5bHhma2NzZ3loZ2hocHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc3NzksImV4cCI6MjA5NTU2Mzc3OY0.DwnwVrOcYfTeiz18BJEvCipoeDUQ4t_dg3biuCtIe94";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZHR5bHhma2NzZ3loZ2hocHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc3NzksImV4cCI6MjA5NTU2Mzc3OX0.DwnwVrOcYfTeiz18BJEvCipoeDUQ4t_dg3biuCtIe94";
 
 // Inicializa o client apenas se a biblioteca existir para evitar travar o escopo global
 const supabaseClient = typeof supabase !== 'undefined' ? supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
